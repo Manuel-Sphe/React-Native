b60f4944950507eaf1abb08368603bc78ee1af56
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2022-06-25 04:52:04 +0200

    Navigation done

--- Navigation/Details.js
+++ Navigation/Details.js
@@ -5,7 +5,18 @@ import styles from "./styles";
 export default function Details({navigation}){
     return(
         <View style = {styles.container}>
-            <Text>{navigation.getParam("title")}</Text>
+            <Text>{navigation.getParam("content")}</Text>
         </View>
     );
 }
+
+Details.navigationOptions = ({navigation}) => ({
+    title: navigation.getParam("title"),
+    headerRight:() =>
+        <Button 
+            title="Buy"
+            onPress={()=>{}}
+            disabled = {navigation.getParam("stock")===0}
+        />
+    
+});
--- Navigation/Home.js
+++ Navigation/Home.js
@@ -8,10 +8,31 @@ export default function Home({ navigation }) {
     return (
         <View style = {styles.container}>
             <Text>Home Page</Text>
-            <Button title="First Item" onPress={() => navigation.navigate("Details",{title:"First Item"})} /> 
-            <Button title="Second Item" onPress={() => navigation.navigate("Details",{title:"Second Item"})}/>
-            <Button title="Third Item" onPress={() => navigation.navigate("Details",{title:"Third Item"})}/>
+            <Button title="First Item" onPress={() => navigation.navigate("Details",{
+                title:"First Item",
+                content:"First Item Content",
+                stock: 1
+                })}
+             /> 
+
+            <Button title="Second Item" onPress={() => navigation.navigate("Details",{
+                title:"Second Item",
+                content:"Second Item Content",
+                stock: 0
+                })}
+            />
+
+            <Button title="Third Item" onPress={() => navigation.navigate("Details",{
+                title:"Third Item",
+                content:"Third Item Content",
+                stock:200
+                })}
+            />
             <Button title="Settings" onPress={() => navigation.navigate("Settings")}/>
         </View>
     );
 }
+
+Home.navigationOptions = {
+    title : "Home Page"
+};
